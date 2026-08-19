88d64ac7ca276ad8b05d961ead110077f74d2012
Yukti Agrawal-creator <yuktiagrawal22@gmail.com>
2026-08-19 12:11:42 +0530

    Fix production CORS and authentication

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, UserProfileView, DashboardView
+
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^register/?$', RegisterView.as_view(), name='register'),
+    re_path(r'^token/?$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(r'^token/refresh/?$', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserProfileView.as_view(), name='user_profile'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
